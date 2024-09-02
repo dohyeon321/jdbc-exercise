941ef8a50997202bde37bd199baa0a015d8bf160
김도현 <sjj2305@naver.com>
2024-09-03 00:04:08 +0900

    add code

--- src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
+++ src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
@@ -14,9 +14,9 @@ public class DbUtils {
     static {
         BasicDataSource basicDataSource = new BasicDataSource();
         //todo#0 {ip},{database},{username},{password} 설정 합니다.
-        basicDataSource.setUrl("jdbc:mysql://{ip}:3306/{database}");
-        basicDataSource.setUsername("{username}");
-        basicDataSource.setPassword("{password}");
+        basicDataSource.setUrl("jdbc:mysql://133.186.241.167:3306/nhn_academy_104");
+        basicDataSource.setUsername("nhn_academy_104");
+        basicDataSource.setPassword("jJ8xngen!");
 
         basicDataSource.setInitialSize(5);
         basicDataSource.setMaxTotal(5);
